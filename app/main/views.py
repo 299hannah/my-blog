795e9573b35699cd4745b516baa7a550384df6a4
299hannah <hannah.wambui@student.moringaschool.com>
2021-06-20 00:18:03 +0300

    imported url_for and validated my routes

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, url_for, flash, redirect
 from . import main
 from .forms import RegForm, LoginForm
 
@@ -26,14 +26,25 @@ def index():
 def about():
     return render_template('main/about.html',title='about')
 
-@main.route('/register')
+@main.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'primary')
+        return redirect(url_for('main.index'))
     return render_template('main/register.html',title='register', form=form)
 
-@main.route('/login')
+@main.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'myblog@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'primary')
+            return redirect(url_for('main.index'))
+
+        else:
+            flash('Log in unsuccessful please check your username or password', 'danger')
+
     return render_template('main/login.html',title='login', form=form)
 
 
